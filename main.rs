b1dcc78e99cc50a68ce7d48e5c847170e4f23cb9
Phil Focke <philfocke@gmail.com>
2026-09-20 21:36:30 +0200

    add basic arity check complete

--- main.rs
+++ main.rs
@@ -1,28 +1,27 @@
 use std::io::{self, BufRead, Write};
-
 enum Reply {
     Pong,
     SimpleString(String),
     BulkString(String),
-    Error(ErrorFormat, String),
+    Error(CommandError),
     //Number can also be float...
     Number(isize),
     NullBulkString(String),
 }
 
-
-enum ErrorFormat {
+enum CommandError {
     UnknownCommand(String),
     WrongNumberOfArguments(String),
 }
-imply ErrorFormat {
-    getFormatString (&self) -> String {
+
+impl CommandError {
+    fn encode(&self) -> String {
         match self {
-            UnknownCommand => {
-                format!("-ERR unknown command '{}'");
-            },
-            WrongNumberOfArguments => {
-                format!("-ERR wrong number of arguments for '{}' command\r\n");
+            Self::UnknownCommand(cmd) => {
+                format!("-ERR unknown command '{}'\r\n", cmd)
+            }
+            Self::WrongNumberOfArguments(cmd) => {
+                format!("-ERR wrong number of arguments for '{}' command\r\n", cmd)
             }
         }
     }
@@ -31,13 +30,19 @@ imply ErrorFormat {
 impl Reply {
     // Hier wäre Gedanke, dass wenn ein Error kommt, geben wir den zurück, sonst geben wir einfach
     // nichts zurück.
-    fn check_arity(String cmd, usize arguments_len) -> Option<Reply>  {
-        let (low, high) = match cmd {
-            "PING" => { (0, 1)},
-            "ECHO" => { (1, 1)},
-            _ => { (0, 100) },
+    fn check_arity(cmd: String, arguments_len: usize) -> Option<Reply> {
+        let (low, high) = match cmd.as_str() {
+            "PING" => (0, 1),
+            "ECHO" => (1, 1),
+            _ => (0, 100),
+        };
+        if arguments_len < low || arguments_len > high {
+            Some(Reply::Error(CommandError::WrongNumberOfArguments(
+                cmd.clone(),
+            )))
+        } else {
+            None
         }
-        if 
         //if Beidnugen => Some else NONE none bedeutet keine fehler und wir gehen weiter.
         //Arity is the number of arguments or operands that a function, operation, or relation takes in logic, mathematics, and computer science
     }
@@ -52,16 +57,23 @@ impl Reply {
 
         //Funktioniert nicht auf den Dingern daher richtig match
         //Genau dasselbe nur mehr verbos
-        //ICh sollte auch erstmal so schreiben...
+
         let (cmd, arguments) = match args.split_first() {
             Some((cmd, arguments)) => (cmd, arguments),
-            None => return Reply::Error("missing cmd".to_string()),
+            None => {
+                return Reply::Error(CommandError::WrongNumberOfArguments(String::from(
+                    "Splitting",
+                )))
+            }
         };
+
+        if let Some(arrity_error) = Reply::check_arity(cmd.to_string(), arguments.len()) {
+            return arrity_error;
+        }
 
         //println!("STUFF: \n {}", cmd.to_uppercase().as_str());
         match cmd.to_uppercase().as_str() {
             "PING" => {
-                
                 //Wenn ein Argument existiert, dann packe es in die Variable und arbeite weiter?
                 //Some ist Optionals in Java nur hole sie mir mit Some raus. und gibt mirkeinen
                 //Error dann umgeschireben if Some(argument) = argument.first {
@@ -72,33 +84,27 @@ impl Reply {
                     Reply::Pong
                 }
             }
-
             "ECHO" => {
-                if let Some(argument) = arguments.first() {
-                    Reply::BulkString(argument.clone())
-                } else {
-                    Reply::Error(ErrorFormat::WrongNumberOfArguments, cmd.clone())
-                }
+                //Wir benutzen hier expect, weil wir schon durch arity check
+                //geguckt haben, dass etwas in first() sitzt als Parameter
+                //wenn es durchgeht dann sollten wir panicen
+                //keine Ifs mehr weil arity check.
+                let argument = arguments
+                    .first()
+                    .expect("Argument is null, even though we arity checked");
+                Reply::BulkString(argument.clone())
             }
             "COMMAND" => {
-                if let Some(argument) = arguments.first() {
-                    if argument == "DOCS" {
-                        Reply::SimpleString("OK".to_string())
-                    } else {
-                        Reply::Error(cmd.clone())
-                    }
+                let argument = arguments
+                    .first()
+                    .expect("Argument is null, even though we arity checked");
+                if argument == "DOCS" {
+                    Reply::SimpleString("OK".to_string())
                 } else {
-                    Reply::Error(cmd.clone())
+                    Reply::Error(CommandError::UnknownCommand(cmd.clone()))
                 }
             }
-            _ => {
-                if let Some(argument) = arguments.first() {
-                    Reply::Error(ErrorFormat::UnknownCommand, argument.clone())
-                } else {
-                    //println!("{:?}", args);
-                    Reply::Error(ErrorFormat::UnknownCommand, cmd.clone())
-                }
-            }
+            _ => Reply::Error(CommandError::UnknownCommand(cmd.clone())),
         }
     }
 
@@ -108,9 +114,7 @@ impl Reply {
             Self::SimpleString(data) => {
                 format!("+{}\r\n", data)
             }
-            Self::Error(data) => {
-                format!("-ERR unknown command '{}'\r\n", data)
-            }
+            Self::Error(data) => data.encode(),
             Self::Number(data) => {
                 format!(":{}\r\n", data)
             }
